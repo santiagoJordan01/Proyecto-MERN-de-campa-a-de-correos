324082c26ae34b2beec5c0b16a201c65b624c549
SantiagoJordan01 <santijordanv@gmail.com>
2025-07-08 14:47:43 -0500

    . .

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import crypto from 'crypto'
 
-// Polyfill para crypto
-globalThis.crypto = crypto
 
 export default defineConfig({
   plugins: [react()],
