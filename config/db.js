f42569b8ff1230028759a1200d20300e02ed8955
Santiago Jordan Vargas <santijordanv@gmail.com>
2026-04-20 22:23:33 -0500

    .

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Render te da esta variable
+  connectionString: process.env.DB_URL, // Render te da esta variable
   ssl: {
     rejectUnauthorized: false, // Necesario para conexiones externas
   },
